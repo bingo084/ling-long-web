8e6b4a164ce884f521e7c1bd508a3a3903c580f3
王斌 <bingo084@outlook.com>
2024-07-17 18:04:49 +0800

    feat: add tdesign-vue-next

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import TDesign from 'tdesign-vue-next'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(TDesign)
 
 app.mount('#app')
